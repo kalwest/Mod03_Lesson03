179ec3a19b5b9bb08bd7291cff5a69392bba57fd
Kaiser Jaffar <kaiserohio2016@gmail.com>
2026-08-04 19:37:02 -0400

    Start Module 03 Lesson 03

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.product_table
+(price, quantity, sku, title_x, created_at, updated_at)
+VALUES
+    (12.4, 4, 'parle734', 'Parle Biscuit', NOW(), NOW()),
+    (14.4, 1, 'pepsi123', 'Pepsi', NOW(), NOW()),
+    (16.4, 3, 'pepsi156', 'Mazza', NOW(), NOW());
